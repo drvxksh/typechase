42c6909f85e6c45c4b109642c2d900f24244c83f
Dhruv Kaushik <dhruvkaushik305@gmail.com>
2025-03-23 22:47:59 +0530

    Add Prettier with Tailwind plugin and update landing page UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,27 @@
-import { Route, Routes } from "react-router";
+import { Link, Route, Routes } from "react-router";
 import { BrowserRouter } from "react-router";
 import Landing from "./Pages/Landing";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" Component={Landing} />
-      </Routes>
-    </BrowserRouter>
+    <main className="flex h-screen w-screen flex-col">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" Component={Landing} />
+        </Routes>
+      </BrowserRouter>
+    </main>
+  );
+}
+
+function Navbar() {
+  return (
+    <nav className="h-[3rem] px-2 py-1">
+      <Link to="/" className="font-[Electrolize] text-2xl font-bold">
+        Type<span className="text-blue-600">chase</span>
+      </Link>
+    </nav>
   );
 }
 
--- frontend/src/pages/landing.tsx
+++ frontend/src/pages/landing.tsx
@@ -1,3 +1,21 @@
+import backgroundImage from "/hero_background.jpg?url";
+
 export default function Landing() {
-  return <main>this is the landing page</main>;
+  return (
+    <section className="flex h-full grow flex-col items-center justify-center">
+      <div className="absolute inset-0 -z-10">
+        <img src={backgroundImage} className="h-full w-full object-cover" />
+      </div>
+      <section>
+        <header className="space-y-2">
+          <h1 className="font-[Roboto] text-5xl font-bold">
+            Fastest fingers first
+          </h1>
+          <h2 className="text-center">
+            A multiplayer typing game to prove your speed
+          </h2>
+        </header>
+      </section>
+    </section>
+  );
 }
