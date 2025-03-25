4258ff6efa4ed38a3697af8d4f73b8183c765fdb
Dhruv Kaushik <dhruvkaushik305@gmail.com>
2025-03-25 18:43:58 +0530

    Add Inter and Raleway fonts and update typography classes

--- frontend/src/pages/landing.tsx
+++ frontend/src/pages/landing.tsx
@@ -8,10 +8,10 @@ export default function Landing() {
       </div>
       <section>
         <header className="flex flex-col items-center justify-center">
-          <h1 className="font-poppins bg-gradient-to-b from-blue-600 to-blue-900 bg-clip-text pb-2 text-center text-4xl font-bold text-transparent sm:text-5xl">
-            Fastest Fingers First
+          <h1 className="font-heading bg-gradient-to-b from-blue-500 to-blue-900 bg-clip-text pb-2 text-center text-4xl font-bold text-transparent sm:text-5xl">
+            Outtype the Competition
           </h1>
-          <h2 className="font-open text-center text-sm font-medium text-zinc-700 sm:text-lg">
+          <h2 className="font-description text-center text-sm font-medium text-zinc-700 sm:text-lg">
             Race against your friends in realtime and discover who truly is the
             fastest.
           </h2>
