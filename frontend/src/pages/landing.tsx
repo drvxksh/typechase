0d79e273d2510d94c83865f87ba1c0d442c9bf9f
Dhruv Kaushik <dhruvkaushik305@gmail.com>
2025-04-04 22:28:36 +0530

    Add lucide-react and redesign landing page interface
    Simplified connecting and failure logic, removed redundant tailwind
    classes

--- frontend/src/pages/landing.tsx
+++ frontend/src/pages/landing.tsx
@@ -1,9 +1,12 @@
+import { useRef } from "react";
 import { useWebSocket } from "../hooks/useWebSocket";
 import backgroundImage from "/hero_background.jpg?url";
+import { Info, ServerCrash } from "lucide-react";
 
 export default function Landing() {
   const { 1: status } = useWebSocket();
   const isConnected = status === "connected";
+  const inviteCodeInputRef = useRef<HTMLInputElement | null>(null);
 
   return (
     <section className="flex h-full grow flex-col items-center justify-center px-4">
@@ -13,52 +16,55 @@ export default function Landing() {
       <section className="flex flex-col items-center gap-5">
         <header className="flex flex-col items-center justify-center">
           <h1 className="font-heading blue-gradient-text bg-clip-text pb-2 text-center text-4xl font-bold text-transparent sm:text-5xl">
-            <span className="inline-block">Outtype the Competition</span>
+            <span className="inline-block">Words At War</span>
           </h1>
           <h2 className="font-description text-center text-sm font-medium text-zinc-700 sm:text-lg">
             Race against your friends in realtime and discover who truly is the
             fastest.
           </h2>
         </header>
-        <div className="flex flex-col items-center gap-1">
-          <form className="w-[60vw] max-w-md">
-            <input
-              type="text"
-              name="gameId"
-              placeholder={
-                isConnected
-                  ? "Join with a code"
-                  : status === "failed"
-                    ? "Unable to connect to game server"
-                    : "Connecting..."
-              }
-              className="w-full rounded-md border-2 border-zinc-200 px-3 py-2 text-xs text-zinc-700 transition-colors duration-200 focus:border-blue-600 focus:outline-none sm:text-sm"
-              disabled={!isConnected}
-            />
-          </form>
-          <p className="text-center text-xs italic">or</p>
-          <button
-            className={`blue-gradient-btn w-[60vw] max-w-md rounded-md px-3 py-2 text-xs text-white transition-transform duration-200 ${
-              isConnected
-                ? "cursor-pointer hover:scale-105 hover:shadow-lg"
-                : "cursor-not-allowed opacity-60"
-            } sm:text-sm`}
-            disabled={!isConnected}
-          >
-            New Game
-          </button>
-          {status === "connecting" && (
-            <p className="mt-2 text-xs text-blue-500">
-              Connecting to server... Please wait.
+        {!isConnected &&
+          (status == "connecting" ? (
+            <p className="flex cursor-wait items-center gap-2">
+              <Info className="size-5 text-blue-500" />
+              <span className="text-sm text-blue-600">
+                Connecting to the server...
+              </span>
             </p>
-          )}
-          {status === "failed" && (
-            <p className="mt-2 text-xs text-red-500">
-              Cannot connect to the server. The service might be down or your
-              internet connection is unstable.
+          ) : (
+            <p className="flex cursor-not-allowed items-center gap-2">
+              <ServerCrash className="size-5 text-zinc-400" />
+              <span className="text-sm text-zinc-600">
+                Could not connect with the server
+              </span>
             </p>
-          )}
-        </div>
+          ))}
+        {isConnected && (
+          <div className="flex w-full flex-col items-center gap-1 px-4 sm:px-15 lg:px-20">
+            <div
+              className="flex w-full cursor-text items-center rounded-lg border-2 border-zinc-200 bg-transparent px-3 py-2 transition-colors duration-300 focus-within:border-zinc-300"
+              onClick={() => {
+                if (inviteCodeInputRef.current)
+                  inviteCodeInputRef.current.focus();
+              }}
+            >
+              <input
+                type="text"
+                name="gameId"
+                placeholder="Enter the invite code..."
+                ref={inviteCodeInputRef}
+                className="w-full text-xs text-zinc-500 focus:outline-none sm:text-sm"
+              />
+              <button className="cursor-pointer text-xs text-zinc-500 underline-offset-2 hover:underline sm:text-sm">
+                Join
+              </button>
+            </div>
+            <p className="text-center text-xs italic">or</p>
+            <button className="blue-gradient-btn w-full cursor-pointer rounded-md px-3 py-2 text-xs text-white transition-transform duration-200 hover:scale-105 hover:shadow-lg sm:text-sm">
+              New Game
+            </button>
+          </div>
+        )}
       </section>
     </section>
   );
