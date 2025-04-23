e53c11a5812b72e3214d11dbe61e5d6866481ca1
Dhruv Kaushik <dhruvkaushik305@gmail.com>
2025-04-23 10:00:54 +0530

    Refactor Game component and improve waiting room UI
    Smoothened the waiting room UI, added heading. Refactored the multiple
    sub-components of the waiting room into one single one. Changed the
    function names to be consistent

--- frontend/src/Pages/Game.tsx
+++ frontend/src/Pages/Game.tsx
@@ -2,33 +2,35 @@ import { Copy, Crown } from "lucide-react";
 import { useParams } from "react-router";
 import { toast } from "sonner";
 import Logo from "../components/Logo";
-import useGameStatus from "../hooks/useGameStatus"; // Ensure this hook is correctly implemented and imported
+import useGameStatus from "../hooks/useGameStatus";
 import useLobbyManagement from "../hooks/useLobbyManagement";
 import { GameStatus } from "../types";
 import { useRef } from "react";
 
 export default function Game() {
-  // fetch the gameId
-  const params = useParams();
-  const gameId = params.gameId;
-
   return (
     <section className="h-full px-4">
       <header className="p-2">
         <Logo />
       </header>
-      <GameContent gameId={gameId} />
+      <RenderGameByStatus />
     </section>
   );
 }
 
 /** A helper function that returns the component based on the status of the game */
-function GameContent({ gameId }: { gameId: string | undefined }) {
+function RenderGameByStatus() {
+  // fetch the gameId
+  const params = useParams();
+  const gameId = params.gameId;
+
+  // validate the gameId and fetch the status and count in case the game is starting
   const { gameStatus, count } = useGameStatus(gameId);
+  // const gameStatus = "starting";
 
   switch (gameStatus) {
     case GameStatus.WAITING:
-      return <LobbyComponent />;
+      return <GameWaiting gameId={gameId as string} />;
     case GameStatus.STARTING:
       return <GameStarting count={count} />;
     case GameStatus.IN_PROGRESS:
@@ -41,19 +43,9 @@ function GameContent({ gameId }: { gameId: string | undefined }) {
 }
 
 /** Rendered when the state of the game is "waiting" */
-function LobbyComponent() {
-  return (
-    <section className="mx-auto mt-[15vh] flex max-w-xl flex-col items-center justify-center gap-5">
-      <InviteCodeInput />
-      <RenderPlayers />
-    </section>
-  );
-}
-
-function InviteCodeInput() {
-  const params = useParams();
-  const gameId = params.gameId as string;
-
+function GameWaiting({ gameId }: { gameId: string }) {
+  const { startGame, leaveGame, changeUsername, lobby } = useLobbyManagement();
+  // TODO implement player name changing
   const handleCopyInviteCode = async () => {
     const copyPromise = navigator.clipboard.writeText(gameId);
     toast.promise(copyPromise, {
@@ -63,24 +55,6 @@ function InviteCodeInput() {
     });
   };
 
-  return (
-    <div
-      className="flex w-full items-center rounded-full px-4 py-2 outline outline-zinc-100"
-      onClick={handleCopyInviteCode}
-    >
-      <input
-        className="w-full cursor-default font-mono text-zinc-700 focus:outline-none"
-        value={gameId}
-        readOnly
-      />
-      <Copy className="size-5 cursor-pointer text-zinc-300 transition-colors duration-300 hover:text-zinc-600" />
-    </div>
-  );
-}
-
-function RenderPlayers() {
-  const { startGame, leaveGame, changeUsername, lobby } = useLobbyManagement();
-
   const handleStartGame = () => {
     startGame();
   };
@@ -89,57 +63,80 @@ function RenderPlayers() {
     leaveGame();
   };
 
-  const inputRef = useRef<HTMLInputElement>(null);
+  const playerNameInputRef = useRef<HTMLInputElement>(null);
 
   const handleChangeUsername = () => {
-    if (inputRef.current) {
-      changeUsername(inputRef.current.value);
+    if (playerNameInputRef.current) {
+      changeUsername(playerNameInputRef.current.value);
     }
   };
 
   const currentUserId = localStorage.getItem("playerId");
-
   const isHost = currentUserId === lobby?.hostId;
 
   return (
-    <section className="flex w-full flex-col gap-3 rounded-lg p-4 outline outline-zinc-100">
-      <div className="flex flex-col space-y-1 divide-y divide-zinc-100">
-        {lobby &&
-          lobby.players.map((item) => (
-            <div className="flex items-center gap-3">
-              <input
-                key={item.playerId}
-                value={item.playerName}
-                className="font-poppins w-full rounded-md p-1 text-sm text-zinc-800 outline outline-zinc-50"
-                readOnly={currentUserId !== item.playerId}
-                ref={handleChangeUsername}
-              />
-              {lobby.hostId === item.playerId && <Crown className="size-5" />}
-            </div>
-          ))}
-      </div>
-      <div className="mx-auto flex w-full gap-2">
-        {isHost && (
-          <button
-            onClick={handleStartGame}
-            className="w-full rounded-md bg-blue-600 text-white"
-          >
-            Start Game
-          </button>
-        )}
-        <button
-          onClick={handleLeaveGame}
-          className="w-full rounded-md bg-red-500 px-4 py-2 text-white"
+    <section className="mx-auto mt-[15vh] flex max-w-xl flex-col items-center justify-center gap-5">
+      <header className="flex w-full flex-col gap-5">
+        <h1 className="font-inter text-center text-3xl font-semibold">
+          Waiting for other players...
+        </h1>
+        <div
+          className="flex w-full cursor-pointer items-center rounded-full px-4 py-2 outline outline-zinc-100"
+          onClick={handleCopyInviteCode}
         >
-          Leave Game
-        </button>
-      </div>
+          <input
+            className="w-full cursor-pointer font-mono text-zinc-700 focus:outline-none"
+            value={gameId}
+            readOnly
+          />
+          <Copy className="size-5 cursor-pointer text-zinc-300 transition-colors duration-300 hover:text-zinc-600" />
+        </div>
+      </header>
+      <section className="flex w-full flex-col gap-3 rounded-xl p-4 outline outline-zinc-100">
+        <div className="flex flex-col space-y-1 divide-y divide-zinc-100">
+          {lobby &&
+            lobby.players.map((item) => (
+              <div className="flex items-center gap-3">
+                <input
+                  key={item.playerId}
+                  value={item.playerName}
+                  className="font-poppins w-full rounded-md p-1 text-sm text-zinc-800 focus:outline-none"
+                  readOnly={currentUserId !== item.playerId}
+                  ref={handleChangeUsername}
+                />
+                {lobby.hostId === item.playerId && <Crown className="size-5" />}
+              </div>
+            ))}
+        </div>
+        <div className="mx-auto flex w-full gap-2">
+          {isHost && (
+            <button
+              onClick={handleStartGame}
+              className="font-inter w-full cursor-pointer rounded-md bg-blue-600 text-white"
+            >
+              Start Game
+            </button>
+          )}
+          <button
+            onClick={handleLeaveGame}
+            className="font-inter w-full cursor-pointer rounded-md bg-red-500 px-4 py-2 text-white"
+          >
+            Leave Game
+          </button>
+        </div>
+      </section>
     </section>
   );
 }
 
 function GameStarting({ count }: { count: string }) {
-  return <section>starting in {count}</section>;
+  return (
+    <section className="flex h-[25vh] items-center justify-center">
+      <h1 className="font-heading text-3xl font-bold sm:text-4xl">
+        starting in {count}
+      </h1>
+    </section>
+  );
 }
 
 function GameInProgress() {
