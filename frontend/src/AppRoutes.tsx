9fb7a19b1878cee2ed7fbd24968931fb7e60e0ad
Dhruv Kaushik <drvxksh@gmail.com>
2025-05-16 21:12:28 +0530

    Move WebSocket logic from hook to context provider

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,17 +1,14 @@
 import { Route, Routes } from "react-router";
 import { Toaster } from "sonner";
 import { WebSocketProvider } from "./context/WebSocketContextProvider";
-import useConnectSocket from "./hooks/useConnectSocket";
 import Game from "./Pages/Game";
 import Landing from "./Pages/Landing";
 import { PlayerProvider } from "./context/PlayerContext";
 
 export default function AppRoutes() {
-  const [socket, status] = useConnectSocket();
-
   return (
     <PlayerProvider>
-      <WebSocketProvider websocket={socket} connectionStatus={status}>
+      <WebSocketProvider>
         <main className="flex h-screen w-screen flex-col">
           <Toaster richColors={true} />
           <Routes>
--- frontend/src/context/WebSocketContextProvider.tsx
+++ frontend/src/context/WebSocketContextProvider.tsx
@@ -1,20 +1,194 @@
 import { ReactNode } from "react";
 import { WebSocketContext } from "../hooks/useWebSocket";
 import { ConnectionStatus } from "../types";
+import { useEffect, useRef, useState } from "react";
+import { useNavigate } from "react-router";
+import { usePlayer } from "./PlayerContext";
+import { toast } from "sonner";
+
+type WebSocketResponse =
+  | {
+      event: "health_check";
+      payload: {
+        message: string;
+      };
+    }
+  | {
+      event: "connect";
+      payload: {
+        playerId: string;
+        existingGameId: string | null;
+      };
+    }
+  | {
+      event: "error";
+      payload: {
+        message: string;
+      };
+    }
+  | {
+      event: "disconnect";
+    };
 
 type ProviderProps = {
-  websocket: WebSocket | null;
-  connectionStatus: ConnectionStatus;
   children: ReactNode;
 };
 
-export function WebSocketProvider({
-  websocket,
-  connectionStatus,
-  children,
-}: ProviderProps) {
+export function WebSocketProvider({ children }: ProviderProps) {
+  const [socket, setSocket] = useState<WebSocket | null>(null);
+  const [status, setStatus] = useState<ConnectionStatus>("connecting");
+  const healthCheckIntervalRef = useRef<number | null>(null);
+  const pendingHealthCheckRef = useRef<boolean>(false);
+
+  const { playerId, setPlayerId, checkAndRestorePlayerId } = usePlayer();
+
+  const navigator = useNavigate();
+
+  useEffect(() => {
+    const newSocket = new WebSocket("ws://localhost:3000");
+    let connectionStatus: ConnectionStatus = "connecting";
+
+    const startHealthCheck = (ws: WebSocket) => {
+      if (healthCheckIntervalRef.current) {
+        clearInterval(healthCheckIntervalRef.current);
+      }
+
+      healthCheckIntervalRef.current = setInterval(() => {
+        if (pendingHealthCheckRef.current) {
+          // we did not hear the health check echo, fail the socket.
+          setStatus("failed");
+
+          ws.close();
+
+          removeHealthCheckListeners();
+        } else if (ws.readyState === WebSocket.OPEN) {
+          pendingHealthCheckRef.current = true;
+
+          ws.send(
+            JSON.stringify({
+              event: "health_check",
+              payload: {},
+            }),
+          );
+        }
+      }, 5000);
+    };
+
+    const removeHealthCheckListeners = () => {
+      if (healthCheckIntervalRef.current) {
+        clearInterval(healthCheckIntervalRef.current);
+      }
+    };
+
+    newSocket.onopen = () => {
+      // First check if playerId in localStorage was deleted and restore it if needed
+      checkAndRestorePlayerId();
+
+      // fetch the existing playerId
+      const existingPlayerId = playerId;
+
+      // connect the backend
+      const payload = {
+        event: "connect",
+        payload: {
+          playerId: existingPlayerId ? existingPlayerId : null,
+        },
+      };
+
+      if (newSocket.readyState === WebSocket.OPEN) {
+        newSocket.send(JSON.stringify(payload));
+      }
+    };
+
+    newSocket.onclose = () => {
+      setStatus("failed");
+
+      removeHealthCheckListeners();
+    };
+
+    newSocket.onmessage = (event: MessageEvent) => {
+      let data: WebSocketResponse | null = null;
+
+      try {
+        data = JSON.parse(event.data);
+      } catch (err) {
+        console.error("couldn't parse the backend response", err);
+
+        connectionStatus = "failed";
+        setStatus(connectionStatus);
+      }
+
+      if (data) {
+        switch (data.event) {
+          case "health_check": {
+            // clear the buffer timeout because the response was received
+            pendingHealthCheckRef.current = false;
+
+            break;
+          }
+          case "connect": {
+            // save the new playerId if it differs from the existing one
+            const newPlayerId = data.payload.playerId;
+            setPlayerId(newPlayerId);
+
+            // update the state variables
+            connectionStatus = "connected";
+            setStatus(connectionStatus);
+            setSocket(newSocket);
+
+            // start the health check after the successfull connection
+            startHealthCheck(newSocket);
+
+            // redirect the player if it was part in the middle of a game
+            if (data.payload.existingGameId) {
+              toast.info("Redirecting you to your old game");
+              navigator(`/game/${data.payload.existingGameId}`);
+            }
+
+            break;
+          }
+          case "error": {
+            toast.error(data.payload.message);
+
+            break;
+          }
+          case "disconnect": {
+            toast.error(
+              "Uh-oh! Connection dropped. You'll need to wait for the game to accept new players to jump back in!",
+            );
+            navigator("/");
+          }
+        }
+      } else {
+        console.error("The server returned a null response");
+
+        connectionStatus = "failed";
+        setStatus(connectionStatus);
+      }
+    };
+
+    newSocket.onerror = () => {
+      // the error is logged anyways when the socket errors, so directly updating the variables without logging
+      connectionStatus = "failed";
+      setStatus(connectionStatus);
+
+      // clean up the timeouts
+      removeHealthCheckListeners();
+    };
+
+    return () => {
+      removeHealthCheckListeners();
+
+      if (newSocket) {
+        newSocket.close();
+      }
+    };
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <WebSocketContext.Provider value={[websocket, connectionStatus]}>
+    <WebSocketContext.Provider value={[socket, status]}>
       {children}
     </WebSocketContext.Provider>
   );
